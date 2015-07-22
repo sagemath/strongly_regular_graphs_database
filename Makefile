5e60c842589d1b3ef6174166b135ac82682c9555
Nathann Cohen <nathann.cohen@gmail.com>
2015-07-22 20:25:10 +0200

    Almost ready for submission

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
-brouwer.txt: brouwer.tmp
-	sage tmp_to_db.py
-	rm -f brouwer.tmp
-
-brouwer.tmp:
+database:
 	./website_to_tmp.sh
-
+	python tmp_to_db.py
+	rm -f brouwer.tmp
 clean:
 	rm -f brouwer.txt
-	rm -f brouwer.sobj
+	rm -f brouwer.json
 	rm -f brouwer.tmp
 
--- tmp_to_db.py
+++ tmp_to_db.py
@@ -21,12 +21,19 @@ data = {(x.pop('v'),x.pop('k'),x.pop('lambda'),x.pop('mu')):x
 with open('brouwer.txt','w') as output:
     for (v,k,l,mu),dic in sorted(data.items()):
         output.write("{:<4} {:<4} {:<4} {:<4} {:<10} {}\n".format(v,k,l,mu,dic['status'],dic['comments']))
-
 print "'brouwer.txt' file written."
 
+# Json output
+import json
+json_list = [[v,k,l,mu,dic['status'],dic['comments']]
+             for (v,k,l,mu),dic in sorted(data.items())]
+with open('brouwer.json', 'w') as output:
+    json.dump(json_list, output)
+print "'brouwer.json' file written."
+
 # sobj output
-from sage.structure.sage_object import save
-save(data,'brouwer.sobj')
+#from sage.structure.sage_object import save
+#save(data,'brouwer.sobj')
 
 print "'brouwer.sobj' file written."
 
